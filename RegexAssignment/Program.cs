6d84af9a805094ebcc7d749560e3bc055ebc8cbc
Ramcharanraj <Ramcharanraj94@gmail.com>
2021-10-16 05:07:02 +0530

    ADDED: Welcome REGEX

---
+++ RegexAssignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RegexAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to REGEX");
+        }
+    }
+}
