9facf689033431924dc6085e43394aa60ba4d8d6
Ramcharanraj <Ramcharanraj94@gmail.com>
2021-10-18 00:34:38 +0530

    ADDED: Validating Email

--- RegexAssignment/Program.cs
+++ RegexAssignment/Program.cs
@@ -19,6 +19,13 @@ namespace RegexAssignment
             Console.WriteLine("Enter The Last Name: ");
             string userLastName = Console.ReadLine();
             LastName.validateLastName(userLastName);
+
+            //Use Case 3
+            Console.WriteLine("Use Case 3");
+            Console.WriteLine("\n");
+            Console.WriteLine("Enter The Email Id: ");
+            string userEmailId = Console.ReadLine();
+            EmailId.validateEmailId(userEmailId);
         }
     }
 }
